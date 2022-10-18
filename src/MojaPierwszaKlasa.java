be873f6413e4f2045efcc03f623ff4ffb048b4f0
Lukasz Kaszczyszyn <lukasz.kaszczyszyn@gmail.com>
2022-10-18 09:23:23 +0200

    my second commit

--- src/MojaPierwszaKlasa.java
+++ src/MojaPierwszaKlasa.java
@@ -2,6 +2,7 @@ public class MojaPierwszaKlasa {
 
     public static void main(String[] args) {
         System.out.println("Poczatek programu");
+        System.out.println("Koniec programu");
     }
 
 }
